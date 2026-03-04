bbf3e389e92bddc4969ab11f7b352028b876e0f0
wildanuwil <wildanfajriyann@gmail.com>
2026-03-04 14:54:01 +0700

    fix typo

--- internal/models/board_member.go
+++ internal/models/board_member.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type BoardMember struct {
-	BoardID  int64     `json:"board_internal_id" db:"board_internal_id" gorm:"primaryKey"`
-	UserID   int64     `json:"user_internal_id" db:"user_internal_id" gorm:"primaryKey"`
-	JoinedAt time.Time `json:"joined_at" db:"joined_at"`
+	BoardInternalID int64     `json:"board_internal_id" db:"board_internal_id" gorm:"primaryKey"`
+	UserInternalID  int64     `json:"user_internal_id" db:"user_internal_id" gorm:"primaryKey"`
+	JoinedAt        time.Time `json:"joined_at" db:"joined_at"`
 }
