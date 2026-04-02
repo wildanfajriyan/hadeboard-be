8e0123793cc57b6ed179bfc44e6c435ff9cfd7d3
wildanuwil <wildanfajriyann@gmail.com>
2026-04-02 19:23:40 +0700

    comment List UpdatedAt

--- internal/models/list.go
+++ internal/models/list.go
@@ -14,5 +14,5 @@ type List struct {
 	BoardPublicId   uuid.UUID `json:"board_public_id" db:"board_public_id"`
 	BoardInternalID int64     `json:"-" db:"board_internal_id"`
 	CreatedAt       time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt       time.Time `json:"updated_at" db:"updated_at"`
+	// UpdatedAt       time.Time `json:"updated_at" db:"updated_at"`
 }
